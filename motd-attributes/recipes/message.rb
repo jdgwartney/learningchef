3a62839813334f21ccbad8e5394806a22905153c
jdgwartney <davidg@boundary.com>
2015-09-01 14:06:47 -0700

    Checkpoint

---
+++ motd-attributes/recipes/message.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: motd-attributes
+# Recipe:: message
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+node.default['motd-attributes']['company'] = 'the best company in the universe'
+
