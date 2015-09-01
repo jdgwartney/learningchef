2970a91b80ba9b7fd4e2bbd70adc104f12ca4a8e
jdgwartney <davidg@boundary.com>
2015-09-01 09:22:20 -0700

    checking artifacts

---
+++ apache/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'Mischa Tayler'
+maintainer_email 'mischa@misheska.com'
+license 'MIT'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
